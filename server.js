56e5af1f0eb2f0e7aa26ecf8dfd229dfd97fdbf2
gerber-corpslink <gerbermecanic@gmail.com>
2026-04-11 08:07:15 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { twiml: { MessagingResponse } } = require('twilio');
+const { MessagingResponse } = require('twilio').twiml;
 
 const app = express();
+
+// Esta es la línea crucial que faltaba para entender los datos de Twilio
 app.use(express.urlencoded({ extended: true }));
 
 app.post('/webhook', (req, res) => {
   console.log('Mensaje recibido:', req.body);
 
   const twiml = new MessagingResponse();
-  twiml.message('🔥 BOT ACTIVO - respuesta correcta');
+  twiml.message('👋 BOT ACTIVO - respuesta correcta');
 
   res.writeHead(200, { 'Content-Type': 'text/xml' });
   res.end(twiml.toString());
